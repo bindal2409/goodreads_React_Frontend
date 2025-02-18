64eb5647ba7b078a57192f544dace36c4b7c83b2
Himanshu <himanshubindal90340@gmail.com>
2025-02-18 17:08:31 +0530

    added routing through react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+import MainRoutes from 'Routes/MainRoutes';
 import './App.css'
-import Home from 'Pages/Home';
 function App() {
   return (
-   <Home/>
+   <MainRoutes/>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import './index.css'
 import App from './App.jsx'
 import store from 'Redux/store'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
+      <BrowserRouter>
     <App />
+    </BrowserRouter>
     </Provider>
 
 )
