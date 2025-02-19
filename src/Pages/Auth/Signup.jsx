9b3e8a10d9e7061b7def06d9f7cb21711220a3b8
Himanshu <himanshubindal90340@gmail.com>
2025-02-19 15:14:15 +0530

    Completed Auth flow

--- src/Pages/Auth/Signup.jsx
+++ src/Pages/Auth/Signup.jsx
@@ -1,6 +1,45 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { signup } from "Redux/Slices/AuthSlice";
 
 export default function Signup(){
+
+        const dispatch= useDispatch();
+        const navigate=useNavigate();
+
+        const [signupDetails,setSignupDetails]=useState({
+            email: '',
+            password: '',
+            username: '',
+        });
+    
+        function handleFormChange(e){
+            const {name,value}=e.target;
+            setSignupDetails({
+                ...signupDetails,
+                [name]:value,
+            })
+            console.log(signupDetails);
+        }
+
+        function resetForm(){
+            setSignupDetails({
+                email: '',
+                password: '',
+                username: ''
+            });
+        }
+    
+        async function onFormSubmit(e) {
+            e.preventDefault();
+                const response = await dispatch(signup(signupDetails));
+                if (response?.payload?.data) {
+                    navigate("/signin");
+                } 
+                resetForm();
+        }
+        
     return (
         <div className="h-[100vh] flex flex-col items-center justify-center">
             <div>
@@ -16,29 +55,38 @@ export default function Signup(){
                 </p>
             </div>
             <div className="w-full">
-                <form className="flex flex-col justify-center items-center w-3/4 mx-auto">
-                    <div className="my-5 w-1/3">
+                <form onSubmit={onFormSubmit} className="flex flex-col justify-center items-center w-3/4 mx-auto">
+                    <div className="my-5 w-1/3 text-black">
                         <input
                             autoComplete="off"
                             type="text"
                             placeholder="username...."
                             className="px-8 py-3 bg-white w-full  text-black"
+                            name="username"
+                            value={signupDetails.username}
+                            onChange={handleFormChange}
                         />
                     </div>
-                    <div className="my-5 w-1/3">
+                    <div className="my-5 w-1/3 text-black">
                         <input
                             autoComplete="off"
                             type="email"
                             placeholder="email...."
                             className="px-8 py-3 bg-white w-full text-black"
+                            name="email"
+                            value={signupDetails.email}
+                            onChange={handleFormChange}
                         />
                     </div>
-                    <div className="my-5 w-1/3">
+                    <div className="my-5 w-1/3 text-black">
                         <input
                             autoComplete="off"
                             type="password"
                             placeholder="password...."
                             className="px-8 py-3 bg-white w-full text-black"
+                            name="password"
+                            value={signupDetails.password}
+                            onChange={handleFormChange}
                         />
                     </div>
                     <div className="my-5 w-1/3">
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import App from './App.jsx'
 import store from 'Redux/store'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <BrowserRouter>
-    <App />
+      <App />
+      <Toaster/>
     </BrowserRouter>
     </Provider>
 
