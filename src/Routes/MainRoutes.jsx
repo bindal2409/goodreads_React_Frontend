b44e8d435f748404857fb1e62ca945397ea39933
Himanshu <himanshubindal90340@gmail.com>
2025-02-20 15:51:51 +0530

    Adde book to shelf

--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -1,8 +1,10 @@
 import Signin from "Pages/Auth/Signin";
 import Signup from "Pages/Auth/Signup";
+import BookDescription from "Pages/BookDescription";
 import Dashboard from "Pages/Dashboard";
 import Home from "Pages/Home";
 import NotFound from "Pages/NotFound";
+import Shelf from "Pages/Shelf";
 import { Route, Routes } from "react-router-dom";
 
 export default function MainRoutes(){
@@ -11,7 +13,9 @@ export default function MainRoutes(){
             <Route path="/" element={<Home/>}></Route>
             <Route path="/signup" element={<Signup/>}></Route>
             <Route path="/signin" element={<Signin/>}></Route>
-            <Route path="/dashboard" element={<Dashboard/>}></Route>  
+            <Route path="/dashboard" element={<Dashboard/>}></Route>
+            <Route path="/shelf" element={<Shelf/>}></Route>
+            <Route path="/book/description" element={<BookDescription/>}></Route>    
             <Route path="*" element={<NotFound/>}></Route>
         </Routes>
     )
